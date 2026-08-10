8d740112ee2f686f4e3ee7c8acf554b8b0054723
younger0126 <younger0126@gmail.com>
2026-08-10 18:39:14 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+window.THE_HEROES_CENTER_CONFIG = {
+  LIFF_ID: "YOUR_OFFICIAL_CENTER_LIFF_ID",
+  API_BASE: "https://the-heroes-api.younger0126.workers.dev",
+  GAME_URL: "https://younger0126.github.io/The-Heroes/",
+  CENTER_NAME: "THE HEROES 官方中心"
+};
