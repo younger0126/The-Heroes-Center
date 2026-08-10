46492c0bd9d7785ba6fc258b9d7819db34df9b91
younger0126 <younger0126@gmail.com>
2026-08-10 19:08:32 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.THE_HEROES_CENTER_CONFIG = {
-  LIFF_ID: "YOUR_OFFICIAL_CENTER_LIFF_ID",
+  LIFF_ID: "2011020275-qs0ajrRg",
   API_BASE: "https://the-heroes-api.younger0126.workers.dev",
   GAME_URL: "https://younger0126.github.io/The-Heroes/",
   CENTER_NAME: "THE HEROES 官方中心"
