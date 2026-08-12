088a2a939b3795ee7a284ff979c36ec6f3ce178d
younger0126 <younger0126@gmail.com>
2026-08-12 19:49:17 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+window.THE_HEROES_CENTER_CONFIG = {
+  LIFF_ID: "2011020275-qs0ajrRg",
+  API_BASE: "https://the-heroes-api.younger0126.workers.dev",
+  GAME_URL: "https://younger0126.github.io/The-Heroes/",
+  CENTER_NAME: "THE HEROES 官方中心",
+  SUPPORT_EMAIL: "YOUR_SUPPORT_EMAIL@example.com",
+  SUPPORT_HOURS: "每日 10:00–22:00",
+  PUBLIC_REVIEW_MODE: true,
+  CENTER_VERSION: "1.2.2"
+};
