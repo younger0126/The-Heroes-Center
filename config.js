a67ac6a1cb5afa5b07a971b359b814a39bd1e791
younger0126 <younger0126@gmail.com>
2026-08-12 19:56:25 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ window.THE_HEROES_CENTER_CONFIG = {
   API_BASE: "https://the-heroes-api.younger0126.workers.dev",
   GAME_URL: "https://younger0126.github.io/The-Heroes/",
   CENTER_NAME: "THE HEROES 官方中心",
-  SUPPORT_EMAIL: "YOUR_SUPPORT_EMAIL@example.com",
+  SUPPORT_EMAIL: "younger0126@gmail.com",
   SUPPORT_HOURS: "每日 10:00–22:00",
   PUBLIC_REVIEW_MODE: true,
   CENTER_VERSION: "1.2.2"
