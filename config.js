2b1412f5db28ee41305de14206c41e4c05623796
younger0126 <younger0126@gmail.com>
2026-08-12 18:55:50 +0800

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-window.THE_HEROES_CENTER_CONFIG = {
-  LIFF_ID: "2011020275-qs0ajrRg",
-  API_BASE: "https://the-heroes-api.younger0126.workers.dev",
-  GAME_URL: "https://younger0126.github.io/The-Heroes/",
-  CENTER_NAME: "THE HEROES 官方中心"
-};
